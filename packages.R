fb519cebdb86f1792fd73b6186cb7c8e8aa6a6d5
Emma Sofie Skarstein <emma@skarstein.no>
2024-11-08 14:36:06 +0100

    cleaned folders, made missing analysis rmd

--- packages.R
+++ packages.R
@@ -1,5 +1,5 @@
-install.packages("aplore3", "tidyverse", "devtools")
+install.packages("aplore3", "tidyverse", "devtools", "patchwork", "plyr")
 
-install.packages("INLA",repos=c(getOption("repos"),INLA="https://inla.r-inla-download.org/R/stable"), dep=TRUE)
+install.packages("INLA", repos=c(getOption("repos"), INLA="https://inla.r-inla-download.org/R/stable"), dep=TRUE)
 devtools::install_github("emmaSkarstein/inlamisclass")
 
